cd7812df28e359247dc934e91a0918eeb49b2179
chs <github@charlie.skilbeck.com>
2024-02-25 14:03:05 +0000

    load_save crc fixed

--- ch554_firmware_midi/src/load_save.c
+++ ch554_firmware_midi/src/load_save.c
@@ -27,7 +27,8 @@ static void save_buffer_set_crc(save_buffer_t *buffer)
 
 static bool save_buffer_check_crc(save_buffer_t const *buffer)
 {
-    return save_buffer_get_crc(buffer) == buffer->crc;
+    uint16 check = save_buffer_get_crc(buffer);
+    return check == buffer->crc;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -51,6 +52,7 @@ bool load_flash(save_buffer_t *buffer)
     if(valid[1] && (index[1] > index[0] || valid[0] == false)) {
         current_save_index = index[1];
         current_save_slot = 1;
+        putstr("load slot 1\n");
         return true;
     }
 
@@ -60,9 +62,11 @@ bool load_flash(save_buffer_t *buffer)
         if(valid[0]) {
             current_save_index = index[0];
             current_save_slot = 0;
+            putstr("load slot 0\n");
             return true;
         }
     }
+    putstr("no save found\n");
     return false;
 }
 
@@ -71,8 +75,10 @@ bool load_flash(save_buffer_t *buffer)
 bool save_flash(save_buffer_t *buffer)
 {
     current_save_index += 1;
-    save_buffer_set_crc(buffer);
     buffer->index = current_save_index;
+    save_buffer_set_crc(buffer);
+    hexdump("save to slot", &current_save_slot, 1);
+    hexdump("save index", &current_save_index, 4);
     uint8 flash_addr = (current_save_slot == 0) ? 0 : sizeof(save_buffer_t);
     current_save_slot = 1 - current_save_slot;
     return write_flash_data(flash_addr, sizeof(save_buffer_t), (uint8 *)buffer);
