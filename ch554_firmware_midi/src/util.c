39b1bfceb7d5132e4e982024e0a3f0aac34d2960
chs <github@charlie.skilbeck.com>
2024-03-02 10:58:03 +0000

    tidy up bootloader

--- ch554_firmware_midi/src/util.c
+++ ch554_firmware_midi/src/util.c
@@ -98,10 +98,6 @@ void led_flash(uint8 n, uint8 speed)
 
 //////////////////////////////////////////////////////////////////////
 
-typedef void (*BOOTLOADER)(void);
-#define bootloader554 ((BOOTLOADER)0x3800)    // CH551/2/3/4
-#define bootloader559 ((BOOTLOADER)0xF400)    // CH558/9
-
 void goto_bootloader()
 {
     // shutdown peripherals
@@ -109,8 +105,9 @@ void goto_bootloader()
     USB_CTRL = 0;
     UDEV_CTRL = 0;
 
+    // flash the led a few times
     led_flash(BOOTLOADER_FLASH_LED_COUNT, BOOTLOADER_FLASH_LED_SPEED);
 
     // and jump to bootloader
-    bootloader554();
+    ((void (*)())BOOT_LOAD_ADDR)();
 }
