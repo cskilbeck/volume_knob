73b233fd1adecc1979c73e2fcc1afc8672c8cba0
chs <github@charlie.skilbeck.com>
2024-03-10 01:18:30 +0000

    swap info/darkmode buttons

--- web_config/web_midi_config/src/App.vue
+++ web_config/web_midi_config/src/App.vue
@@ -60,6 +60,7 @@ if (got_midi.value) {
             @click="midi.init_devices()">
             Scan
           </button>
+          <DarkMode />
           <button @click="closeableModal = true" class="btn btn-sm">
             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-info-circle"
               viewBox="0 0 16 16">
@@ -68,7 +69,6 @@ if (got_midi.value) {
                 d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
             </svg>
           </button>
-          <DarkMode />
         </div>
       </div>
     </div>
